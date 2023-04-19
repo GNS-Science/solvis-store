d51e404e0178c9078458967994d8cc8f38e6a502
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-04-19 12:39:34 +1200

    update authors; remove geopandas

--- solvis_store/__init__.py
+++ solvis_store/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Solvis Store."""
 
-__author__ = """Oakley"""
-__email__ = 'o.jurgens@gns.cri.nz'
+__author__ = """NSHM project @ GNS Science"""
+__email__ = 'nshm@gns.cri.nz'
 __version__ = '1.0.0'
